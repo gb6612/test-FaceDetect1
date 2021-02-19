98c466e8075d08c025c0c2b791f6ba94af789f87
Gabri <gbellini@betech.ch>
2021-02-19 07:41:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace test_FaceDetect1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.fpsLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(12, 12);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(75, 23);
+            this.connect_button.TabIndex = 0;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(36, 95);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(342, 225);
+            this.videoSourcePlayer.TabIndex = 1;
+            this.videoSourcePlayer.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // fpsLabel
+            // 
+            this.fpsLabel.AutoSize = true;
+            this.fpsLabel.Location = new System.Drawing.Point(177, 17);
+            this.fpsLabel.Name = "fpsLabel";
+            this.fpsLabel.Size = new System.Drawing.Size(35, 13);
+            this.fpsLabel.TabIndex = 2;
+            this.fpsLabel.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(384, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 312);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fpsLabel);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.connect_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect_button;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Label fpsLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+// REMEMBER TO IMPORT PACKAGE NuGet -> Emgu.CV.Bitmap
+// and Emgu.CV.runtime.windows
+
+namespace test_FaceDetect1
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopWatch = null;
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            VideoCaptureDeviceForm form = new VideoCaptureDeviceForm();
+
+            if (form.ShowDialog(this) == DialogResult.OK)
+            {
+                // create video source
+                VideoCaptureDevice videoSource = form.VideoDevice;
+
+                // open it
+                OpenVideoSource(videoSource);
+            }
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            CloseCurrentVideoSource();
+
+            // start new video source
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start();
+
+            // reset stop watch
+            stopWatch = null;
+
+            // start timer
+            timer1.Start();
+
+            this.Cursor = Cursors.Default;
+        }
+
+        // Close video source if it is running
+        private void CloseCurrentVideoSource()
+        {
+            if (videoSourcePlayer.VideoSource != null)
+            {
+                videoSourcePlayer.SignalToStop();
+                videoSourcePlayer.WaitForStop();
+
+                
+                if (videoSourcePlayer.IsRunning)
+                {
+                    videoSourcePlayer.Stop();
+                }
+
+                videoSourcePlayer.VideoSource = null;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IVideoSource videoSource = videoSourcePlayer.VideoSource;
+
+            if (videoSource != null)
+            {
+                // get number of frames since the last timer tick
+                int framesReceived = videoSource.FramesReceived;
+
+                if (stopWatch == null)
+                {
+                    stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                }
+                else
+                {
+                    stopWatch.Stop();
+
+                    float fps = 1000.0f * framesReceived / stopWatch.ElapsedMilliseconds;
+                    fpsLabel.Text = fps.ToString("F2") + " fps";
+
+                    stopWatch.Reset();
+                    stopWatch.Start();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            Bitmap mybitmap = image;
+
+            Image<Bgr, byte> greyImage = mybitmap.ToImage<Bgr, byte>(); // REMEMBER TO IMPORT PACKAGE NuGet -> Emgu.CV.Bitmap
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(greyImage, 1.2, 1);
+            foreach (Rectangle rectangle in rectangles)
+            {
+                using (Graphics graphics = Graphics.FromImage(mybitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pictureBox1.Image = mybitmap;
+        }
+    }
+}
